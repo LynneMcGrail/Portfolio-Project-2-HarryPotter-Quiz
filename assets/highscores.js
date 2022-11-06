6d3206cba10db064fe10736214fb98431112f236
Lynne McGrail <lynne.mcgrail@gmail.com>
2022-11-06 20:49:02 +0000

    remove conflicting highscoreslist const

--- assets/highscores.js
+++ assets/highscores.js
@@ -2,9 +2,9 @@ const username = document.getElementById('username');
 const saveButton = document.getElementById('saveScoreBtn');
 const highScoresList = document.querySelector('#highScoresList');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []; // variable that creates an array for the most recent high scores
-const highScoresList = document.getElementById('highScoresList');
 
 highScoresList.innerHTML = 
 highScores.map(score => {
     return `<li class="high-score">${score.name} - ${score.score}</li>`
 }).join("")
+
