42536f9b147219b33d56282f9c978269ca3621eb
Lynne McGrail <lynne.mcgrail@gmail.com>
2022-11-06 18:31:09 +0000

    fix usse with highScores in end js file

--- assets/game.js
+++ assets/game.js
@@ -129,10 +129,10 @@ startGame = () => {
 console.error("You made a mistake");
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+    if(availableQuestions.length == 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('../end.html')
+        return window.location.assign('end.html')
     }
 
     questionCounter++
