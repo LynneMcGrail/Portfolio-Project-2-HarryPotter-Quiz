6f928c079c92e7dd93ac509e6341c22cfaac3ae3
Lynne McGrail <lynne.mcgrail@gmail.com>
2022-11-08 16:20:24 +0000

    format document highscores.js

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -3,8 +3,7 @@ const saveButton = document.getElementById('saveScoreBtn');
 const highScoresList = document.querySelector('#highScoresList');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []; // variable that creates an array for the most recent high scores
 
-highScoresList.innerHTML = 
-highScores.map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`
-}).join("")
-
+highScoresList.innerHTML =
+    highScores.map(score => {
+        return `<li class="high-score">${score.name} - ${score.score}</li>`
+    }).join("")
